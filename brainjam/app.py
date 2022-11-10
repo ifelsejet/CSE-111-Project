5e37944714e2f6da1297b73fdc8ecb82ecd79856
Jatnael Montes <ifelsejet@gmail.com>
2022-11-10 15:00:40 -0800

    added basic routes

--- brainjam/app.py
+++ brainjam/app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/lobby")
+def hello_lobby():
+    return "<p>Welcome to Lobby</p>"
+
+@app.route("/game")
+def hello_game():
+    return "<p>Welcome to the game</p>"
