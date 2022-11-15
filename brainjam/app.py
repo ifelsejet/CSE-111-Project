8229381ac6f7ca5406c75ae20901f38b23f184b3
Jatnael Montes <ifelsejet@gmail.com>
2022-11-14 17:35:57 -0800

    Added basic bootstrap, need to link pages together

--- brainjam/app.py
+++ brainjam/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
+def home():
+    return render_template('home.html')
+@app.route("/createRoom")
+def createRoom():
+    return render_template('create.html')
 @app.route("/lobby")
 def hello_lobby():
     return "<p>Welcome to Lobby</p>"
