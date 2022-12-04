a5751600261ad7a019a2d53e57f62e58c923d402
Jatnael Montes <ifelsejet@gmail.com>
2022-12-03 16:24:40 -0800

    temp push

--- brainjam/app.py
+++ brainjam/app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///basketball.db'
+ 
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+# Models
+class Question_Types(db.Model):
+    # qt_id : Field which stores unique id for every row in
+    # database table.
+    # qt_type: Used to store the question type
+   
+    qt_id = db.Column(db.Integer, primary_key=True)
+    qt_type = db.Column(db.String(20), unique=False, nullable=False)
+   
+ 
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Question Types : {self.qt_type}"
+ 
 
 @app.route("/")
 def home():
@@ -14,4 +38,4 @@ def hello_lobby():
 
 @app.route("/game")
 def hello_game():
-    return "<p>Welcome to the game</p>"
+    return render_template('game.html')
